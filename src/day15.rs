9c2a6f08568a99ac81eb351e3cc6ff617bb9a977
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2020-12-15 08:35:25 -0500

    fixup

--- src/day15.rs
+++ src/day15.rs
@@ -9,8 +9,7 @@ fn to_vec(input: &str) -> Vec<usize> {
         .collect()
 }
 
-#[aoc(day15, part1)]
-fn number_spoken(input: &Vec<usize>) -> usize {
+fn last_spoken(input: &Vec<usize>, last: usize) -> usize {
     let mut turns_spoken: HashMap<usize, usize> = input
         .iter()
         .take(input.len() - 1)
@@ -18,7 +17,7 @@ fn number_spoken(input: &Vec<usize>) -> usize {
         .map(|(i, x)| (*x, i))
         .collect();
     let mut last_spoken = *input.last().unwrap();
-    for i in input.len()..=2019 {
+    for i in input.len()..last {
         let maybe_spoken = turns_spoken.get(&last_spoken);
         let newly_spoken = match maybe_spoken {
             Some(last_time) => i - *last_time - 1,
@@ -30,25 +29,14 @@ fn number_spoken(input: &Vec<usize>) -> usize {
     return last_spoken;
 }
 
+#[aoc(day15, part1)]
+fn number_spoken(input: &Vec<usize>) -> usize {
+    last_spoken(input, 2020)
+}
+
 #[aoc(day15, part2)]
 fn number_spoken_big(input: &Vec<usize>) -> usize {
-    let mut turns_spoken: HashMap<usize, usize> = input
-        .iter()
-        .take(input.len() - 1)
-        .enumerate()
-        .map(|(i, x)| (*x, i))
-        .collect();
-    let mut last_spoken = *input.last().unwrap();
-    for i in input.len()..=29999999 {
-        let maybe_spoken = turns_spoken.get(&last_spoken);
-        let newly_spoken = match maybe_spoken {
-            Some(last_time) => i - *last_time - 1,
-            None => 0,
-        };
-        turns_spoken.insert(last_spoken, i - 1);
-        last_spoken = newly_spoken as usize;
-    }
-    return last_spoken;
+    last_spoken(input, 30000000)
 }
 
 #[cfg(test)]
@@ -65,7 +53,5 @@ mod tests {
     #[test]
     fn test_number_spoken_big() {
         assert_eq!(number_spoken_big(&to_vec("0,3,6")), 175594);
-        assert_eq!(number_spoken_big(&to_vec("1,3,2")), 2578);
-        assert_eq!(number_spoken_big(&to_vec("1,2,3")), 261214);
     }
 }
